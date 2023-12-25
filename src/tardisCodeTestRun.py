229238dabf3760155275a9ebc688d1ad7cb5de89
Eric Liu <eric7liu562@gmail.com>
2023-12-24 20:46:05 -0500

    changeDataCalling

--- src/tardisCodeTestRun.py
+++ src/tardisCodeTestRun.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
 # Reading directly from a .csv.gz file
-data = pd.read_csv(r'C:\workspace\dqn\mywork\datasets\binance_book_snapshot_25_2023-04-14_BTCUSDT.csv.gz', compression='gzip')
+data = pd.read_csv(r'C:\Users\ehild\Desktop\dqn_backup\datasets\binance_book_snapshot_25_2023-04-14_ADAUSDT.csv.gz', compression='gzip')
 
 print(data)
